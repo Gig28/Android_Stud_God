2f8bce92e99607fe1d75db240943381df27e9093
gig28 <rrgig28@gmail.com>
2023-03-30 09:59:47 +0300

    Implicit Intent. Отправляем заказ по email

--- app/src/main/java/com/example/ast/OrderActivity.java
+++ app/src/main/java/com/example/ast/OrderActivity.java
@@ -3,10 +3,16 @@ package com.example.ast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
+
+    String[] addresses = {" rrgig28@gmail.com "};
+    String subject = "Order from Music Shop";
+    String emailText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,13 @@ public class OrderActivity extends AppCompatActivity {
         double Price = orderIntent.getDoubleExtra("Price",0);
         double orderPrice = orderIntent.getDoubleExtra("OrderPrice",0);
         TextView orderTextView = findViewById(R.id.textView2);
+
+        emailText = "Имя клиента: "+ userName + "\n" +
+                "Товар: " +  goodsName + "\n" +
+                "Кол-во: " + quantity + " шт" +"\n" +
+                "Цена за шт: " + Price + "\n" +
+                "Итоговая стоимость: " + orderPrice;
+
         orderTextView.setText("Имя клиента: "+ userName + "\n" +
                "Товар: " +  goodsName + "\n" +
                "Кол-во: " + quantity + " шт" +"\n" +
@@ -30,5 +43,15 @@ public class OrderActivity extends AppCompatActivity {
 
 
 
+    }
+
+    public void submitOrder(View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should hanc
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, emailText);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);}
     }
 }
